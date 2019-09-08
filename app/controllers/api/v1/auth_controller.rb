fcd7731453466df48c65abd448a3f0809f3d19eb
Justin <justinhwu95@gmail.com>
2019-09-08 19:16:41 -0400

    Created encoding and decoding methods for the secret key

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -2,9 +2,15 @@ class UsersController < ApplicationController
    def create
      @user = User.find_by(username: params[:username], password: params[:password])
      if @user && user.authenticate(params[:password])
-       render json: @user
+       render json: {
+         user: @user,
+         authenticated: true
+       }, status: :ok
      else
-       render json: {error: "Your Username or Password is Incorrect."}
+       render json: {
+         message: "Your Username or Password is Incorrect.",
+         authenticated: true
+       }, status: :unauthorized
      end
    end
 end
